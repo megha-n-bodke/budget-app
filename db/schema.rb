5607f8bd4d2a2bdf580a7b764016a98277672399
Megha Bodke <megha.n.bodke@gmail.com>
2023-01-12 21:06:25 +0530

    Add helper model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_09_105246) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_12_153354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_105246) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "group_categories", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_group_categories_on_category_id"
+    t.index ["group_id"], name: "index_group_categories_on_group_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -50,5 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_105246) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "group_categories", "categories"
+  add_foreign_key "group_categories", "groups"
   add_foreign_key "groups", "users"
 end
