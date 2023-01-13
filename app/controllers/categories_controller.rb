d1f3cd0dd2a97e205777d59a018aa1e8fdda4645
Megha Bodke <megha.n.bodke@gmail.com>
2023-01-13 21:10:14 +0530

    Add category functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,24 @@
 class CategoriesController < ApplicationController
+ 
   def new
     @category = Group.new
   end
 
   def create
     @category = Group.new(category_params)
+    @category.user_id = current_user.id
     if @category.save
       redirect_to root_path, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @category = Group.find(params[:id])
+    @category.destroy
+    if @category.destroy
+      redirect_to root_path
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'categories/new'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "spalsh_screen#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # root "spalsh_screen#index"
-  root "categories#new"
-    resources :categories, only: [:index, :show, :create, :destroy, :new] do
+  resources :categories, only: [:index, :show, :create, :destroy, :new] do
     resources :transaction, only: [:index, :create, :destroy, :new]
   end
 end
