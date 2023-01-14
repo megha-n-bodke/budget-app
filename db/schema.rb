89d0ef12a2bb4d392b6dc415c81f4ce4c1a067fe
Megha Bodke <megha.n.bodke@gmail.com>
2023-01-14 21:37:28 +0530

    Change amount type

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_12_153354) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_14_153850) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.string "amount"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "amount"
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
@@ -50,10 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_153354) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
